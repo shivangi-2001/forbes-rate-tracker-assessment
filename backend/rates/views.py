dadfb12d5b8b90159e56a329b52fa67365185986
shivangi <shivangi@scotty-world.com>
2026-04-02 18:55:07 +0530

    backend lint

--- backend/rates/views.py
+++ backend/rates/views.py
@@ -1,5 +1,4 @@
 import logging
-import time
 
 from django.core.cache import cache
 from django.utils.dateparse import parse_date
@@ -8,7 +7,6 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.response import Response
 
 from .models import Rate
 from .serializers import RateIngestSerializer, RateSerializer
